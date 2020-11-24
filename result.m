b31311d6f088cf0ba68ef88f6c776cee0df3653d
jelly0422 <1187893119@qq.com>
2020-11-24 17:56:41 +0800

    通过人脸标签得到名字

---
+++ result.m
@@ -0,0 +1,30 @@
+function name=result(PreLabel)
+    switch(PreLabel)
+        case 1
+            name='边伯贤';
+        case 2
+            name='都暻秀';
+        case 3
+            name='黄子韬';
+        case 4
+            name='金俊勉';
+        case 5
+            name='金珉锡';
+        case 6
+            name='金钟大';
+        case 7
+            name='金钟仁';
+        case 8
+            name='鹿晗';
+        case 9
+            name='朴灿烈';
+        case 10
+            name='吴世勋';
+        case 11
+            name='吴亦凡';
+        case 12
+            name='张艺兴';
+        otherwise
+            name='数据集中无此人';
+    end
+    
